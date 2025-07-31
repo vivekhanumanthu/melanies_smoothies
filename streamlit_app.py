ed8bdbff303fd762984b1119579b278d8643b50b
vivekhanumanthu <133838397+vivekhanumanthu@users.noreply.github.com>
2025-07-31 16:19:56 +0530

    for fruits choosen only api works

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 # from snowflake.snowpark.context import get_active_session
 
@@ -28,6 +29,8 @@ if ingredients_list:
 
     for fruit_choosen in ingredients_list:
         ingredients_string+=fruit_choosen+' '
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.text(smoothiefroot_response.json())
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
@@ -38,6 +41,5 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-import requests
-smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+
+
