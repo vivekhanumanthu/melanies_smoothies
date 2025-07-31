9f26e6c88fb5b88be28c98b0501e6ee9d2d022cf
vivekhanumanthu <133838397+vivekhanumanthu@users.noreply.github.com>
2025-07-31 16:52:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,17 +20,15 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list=st.multiselect(
     "choose upto 5 ingredients:",
-    my_dataframe
+    my_dataframe,
+    max_selections=5
 )
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text(ingredients_list)
-
     ingredients_string=''
 
     for fruit_choosen in ingredients_list:
@@ -38,7 +36,7 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_choosen+' Nutrition Info')
-        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choosen)
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
